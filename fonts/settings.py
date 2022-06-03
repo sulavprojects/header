937f5bc1a62001397dcec415a75ee3326713f962
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-03 07:54:07 +0545

    first

--- fonts/settings.py
+++ fonts/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7e2y8_gmmswkqy(ngtbg#mdrw%l2$xhybj(f^wqk)+8-zo_ddq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arabfonts.herokuapp.com', 'local', '*']
 
 
 # Application definition
